9cfaa13cdb6ed2a2e16a9eb26a03da8a200358b5
Julian Smith <julian.smith@artifex.com>
2026-06-18 17:31:49 +0100

    setup.py: increment version to 9.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def sdist():
 
 p = pipcl.Package(
         'pipcl',
-        version = '8',
+        version = '9',
         pure = True,
         requires_dist='packaging',
         description='README.rst',
