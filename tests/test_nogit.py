e5888a3ec192ca9e49670b246fc737a30ac1b7cd
Julian Smith <julian.smith@artifex.com>
2026-07-08 22:41:02 +0100

    tests/test_nogit.py: avoid spurious test failure on github.

--- tests/test_nogit.py
+++ tests/test_nogit.py
@@ -1,19 +1,26 @@
-import pipcl
-
+import os
 import tempfile
 
+import pipcl
 
-# Check pipcl.git_info_py() in a non-git-checkout.
 
 def test_nogit_check():
-    path = tempfile.gettempdir()
-    try:
-        text = pipcl.git_info_py(path)
-    except Exception:
-        text = None
-    else:
-        assert 0, f'Expected exception for directory: {path}'
-    print(f'{text}')
+    '''
+    Check pipcl.git_info_py() in a non-git-checkout.
+    '''
+    GITHUB_ACTIONS = os.environ.get('GITHUB_ACTIONS')
+    if GITHUB_ACTIONS == 'true':
+        print(f'test_nogit_check(): not running on Github because tempdir is within checkout.')
+        return
+    with tempfile.TemporaryDirectory() as path:
+        try:
+            text = pipcl.git_info_py(path)
+        except Exception:
+            text = None
+        else:
+            print(f'{text=}')
+            assert 0, f'Expected exception for directory: {path}'
+    print(f'{text=}')
 
 
 def test_nogit_nocheck():
