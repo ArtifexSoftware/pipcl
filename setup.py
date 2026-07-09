e1a2c5e794920af558e35558dd5b59f62165e3da
Julian Smith <julian.smith@artifex.com>
2026-07-09 06:29:35 +0100

    setup.py: incremented version to 12.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def sdist():
 
 p = pipcl.Package(
         'pipcl',
-        version = '11',
+        version = '12',
         pure = True,
         requires_dist='packaging',
         description='README.rst',
